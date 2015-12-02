1c11a61a74542028499761dd1fcdf5508798d819
TMuller <thomas.muller@concur.com>
2015-12-02 15:10:03 +0000

    test

--- src/main/scala/com/qa/DoNothing.java
+++ src/main/scala/com/qa/DoNothing.java
@@ -4,7 +4,7 @@ public class DoNothing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Nothing 2 - test commit
+		// Nothing 2 - test commit test 3
 	}
 
 }
